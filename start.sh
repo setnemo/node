ebcac9c9a36ed028501abb1909b0fde10728b724
Artem Pakhomov <setnemo@gmail.com>
2024-04-09 21:17:45 +0300

    update building latest node image

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Run custom scripts
-if [[ "$RUN_SCRIPTS" == "1" ]] ; then
-  if [ -d "/var/www/html/scripts/" ]; then
-    # make scripts executable incase they aren't
-    chmod -Rf 750 /var/www/html/scripts/*; sync;
-    # run scripts in number order
-    for i in `ls /var/www/html/scripts/`; do /var/www/html/scripts/$i ; done
-  else
-    echo "Can't find script directory"
-  fi
-fi
-
-
-# Start supervisord and services
+/usr/sbin/crond -b
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
